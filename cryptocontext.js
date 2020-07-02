7c42a210b12f65261723f6a566812a02d744f061
lovemjj <516370418@qq.com>
2020-07-02 09:06:58 +0800

    pub compression

--- cryptocontext.js
+++ cryptocontext.js
@@ -3,19 +3,18 @@ module.exports = function CryptoContext() {
     const sm2 = require('sm-crypto').sm2;
     const sm3 = require('sm-crypto').sm3;
     const sm4 = require('sm-crypto').sm4;
+    const BigInteger = require('bigi');
+    const ecurve = require('ecurve');
 
     this.generateKeyPair = function(){
         let keypair = sm2.generateKeyPairHex();
-        let publicKey = keypair.publicKey; // 公钥
         let privateKey = keypair.privateKey; // 私钥
+        let publicKey = sm2.getBcecPublicKeyFromPrivateKey(privateKey); // 公钥
         return [privateKey, publicKey]
     }
 
     this.getPublicKeyFromPrivateKey= function(sk){
-        let keypair = sm2.generateKeyPairHex();
-        let publicKey = keypair.publicKey; // 公钥
-        let privateKey = keypair.privateKey; // 私钥
-        return sm2.getPublicKey()
+        return sm2.getBcecPublicKeyFromPrivateKey(sk)
     }
 
     this.sign = function(sk, msg){
@@ -27,6 +26,7 @@ module.exports = function CryptoContext() {
     }
 
     this.verify = function(pk, msg, sig){
+        var privateKey = new Buffer(sk, 'hex')
         return sm2.doVerifySignature(msg, sig, pk, {
             der: true,
             hash: true,
--- test.js
+++ test.js
@@ -2,20 +2,23 @@
 const cryptoContext = require('./CryptoContext')
 const sm2 = require('sm-crypto').sm2
 
-// const msg = "5669b294071363ea51d09206f0b02426a6457d3bd6fc1fa72d4f188a30821fed"
+const msg = "5669b294071363ea51d09206f0b02426a6457d3bd6fc1fa72d4f188a30821fed"
 // const plain = [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0x45, 0x67, 0x89, 0xab, 0x89, 0xab, 0x45, 0x67, 0x89, 0xab]
 // const key = [0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10]
 cryptocontext = new cryptoContext()
-// let keypair = cryptocontext.generateKeyPair()
-// pk = keypair[1] // 公钥
-// sk = keypair[0] // 私钥
-// console.log(pk)
-// console.log(sk)
+let keypair = cryptocontext.generateKeyPair()
+pk = keypair[1] // 公钥
+sk = keypair[0] // 私钥
+console.log(pk)
+console.log(sk)
 // sk = "24494d955e8dc18ee3d2b715a9789dd05d9571fe0c17b7f20b697ace21cfa20b"
+// sk = "1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd"
 // console.log(sm2.getBcecPublicKeyFromPrivateKey(sk))
 // console.log(sm2.getPublicKeyFromBcecPublicKey(sm2.getBcecPublicKeyFromPrivateKey(sk)))
 // sign = cryptocontext.sign(sk, msg)
 // verify = cryptocontext.verify(pk, msg, sign)
+// console.log(sign)
+// console.log(verify)
 // encryptData = cryptocontext.encrypt(key, plain)
 // decryptData = cryptocontext.decrypt(key, encryptData)
 //
@@ -43,43 +46,71 @@ cryptocontext = new cryptoContext()
 // console.log(JSON.stringify(plain))
 // console.log(cryptocontext.sm3('abc'))
 
-const { randomBytes } = require('crypto')
-const secp256k1 = require('secp256k1')
+// const { randomBytes } = require('crypto')
+// const secp256k1 = require('secp256k1')
 // or require('secp256k1/elliptic')
 //   if you want to use pure js implementation in node
 //
 // generate message to sign
 // message should have 32-byte length, if you have some other length you can hash message
 // for example `msg = sha256(rawMessage)`
-const msg = randomBytes(32)
+// const msg = randomBytes(32)
+//
+// // generate privKey
+// let privKey
+// do {
+//     privKey = randomBytes(32)
+// } while (!secp256k1.privateKeyVerify(privKey))
+//
+// // get the public key in a compressed format
+// const pubKey = secp256k1.publicKeyCreate(privKey)
+//
+// // sign the message
+// const sigObj = secp256k1.ecdsaSign(msg, privKey)
+// function hashfn (x, y) {
+//     const pubKey = new Uint8Array(33)
+//     pubKey[0] = (y[31] & 1) === 0 ? 0x02 : 0x03
+//     pubKey.set(x, 1)
+//     return pubKey
+// }
+//
+// // get X point of ecdh
+// const ecdhPointX = secp256k1.ecdh(pubKey, privKey, { hashfn }, Buffer.alloc(33))
+// console.log(ecdhPointX.toString('hex'))
+// console.log(typeof msg)
+// console.log(msg.toString('hex'))
+// console.log(privKey.toString('hex'))
+// console.log(sm2.getBcecPublicKeyFromPrivateKey(privKey.toString('hex')))
+// console.log(Buffer.from(pubKey).toString('hex'))
+// console.log(Buffer.from(sigObj.signature).toString('hex'))
+// console.log(cryptocontext.sign(privKey.toString('hex'), msg.toString('hex')))
+// // verify the signature
+// console.log(secp256k1.ecdsaVerify(sigObj.signature, msg, pubKey))
 
-// generate privKey
-let privKey
-do {
-    privKey = randomBytes(32)
-} while (!secp256k1.privateKeyVerify(privKey))
+// Generate keys
+// var crypto = require('crypto')
+//
+// var BigInteger = require('bigi') //npm install --save bigi@1.1.0
+// var ecurve = require('ecurve') //npm install --save ecurve@1.0.0
+// console.log(sm2.getBcecPublicKeyFromPrivateKey(sk))
+// var privateKey = new Buffer(sk, 'hex')
+// console.log(privateKey)
+// var ecparams = ecurve.getCurveByName('ec')
+// var curvePt = ecparams.G.multiply(BigInteger.fromBuffer(privateKey))
+// console.log(curvePt)
+// var x = curvePt.affineX.toBuffer(32)
+// console.log(sm2.getBcecPublicKeyFromPrivateKey(sk))
+// console.log(x.toString('hex'))
+// console.log(ecurve.Curve.prototype.pointFromX(sm2.getBcecPublicKeyFromPrivateKey(sk)[0] === 0x03, sm2.getBcecPublicKeyFromPrivateKey(sk)))
+// var y = curvePt.affineY.toBuffer(32)
+// console.log(y.toString('hex'))
+const sec = require('ecc-jsbn/lib/sec')
+var curve = getSECCurveByName("secp256r1").getCurve(); // from sec.js
 
-// get the public key in a compressed format
-const pubKey = secp256k1.publicKeyCreate(privKey)
+var compressed = "028bd9aaec3783a891b4d4707004b265715651a6a78f6a43b30c2b51d0b63052d8";
+var decompressed = curve.decodePointHex(compressed); // of type ECPointFp
+compressed = curve.encodeCompressedPointHex(decompressed);
+decompressed = curve.decodePointHex(compressed);
 
-// sign the message
-const sigObj = secp256k1.ecdsaSign(msg, privKey)
-function hashfn (x, y) {
-    const pubKey = new Uint8Array(33)
-    pubKey[0] = (y[31] & 1) === 0 ? 0x02 : 0x03
-    pubKey.set(x, 1)
-    return pubKey
-}
-
-// get X point of ecdh
-const ecdhPointX = secp256k1.ecdh(pubKey, privKey, { hashfn }, Buffer.alloc(33))
-console.log(ecdhPointX.toString('hex'))
-console.log(typeof msg)
-console.log(msg.toString('hex'))
-console.log(privKey.toString('hex'))
-console.log(sm2.getBcecPublicKeyFromPrivateKey(privKey.toString('hex')))
-console.log(Buffer.from(pubKey).toString('hex'))
-console.log(Buffer.from(sigObj.signature).toString('hex'))
-console.log(cryptocontext.sign(privKey.toString('hex'), msg.toString('hex')))
-// verify the signature
-console.log(secp256k1.ecdsaVerify(sigObj.signature, msg, pubKey))
+console.log(compressed);
+console.log(curve.encodePointHex(decompressed)); // uncompressed hex encoding
