11d0916fadcfb2e5c029150bfe0bef83742dc2ca
lovemjj <516370418@qq.com>
2020-07-03 15:33:37 +0800

    Update test.js

--- test.js
+++ test.js
@@ -5,8 +5,10 @@ cryptocontext = new cryptoContext()
 let keypair = cryptocontext.generateKeyPair()
 pk = keypair[1] // 公钥
 sk = keypair[0] // 私钥
+let pubfromsk = cryptocontext.getPublicKeyFromPrivateKey(sk)
 console.log(sk)
 console.log(pk)
+console.log(pubfromsk)
 sign = cryptocontext.sign(sk, msg)
 verify = cryptocontext.verify(pk, msg, sign)
 console.log(verify)
